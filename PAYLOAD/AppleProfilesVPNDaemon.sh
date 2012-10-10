7bd5a271c7bbd84148727a91e1f548f7c5ff5261
Chris Gerke <chris.gerke@gmail.com>
2012-10-11 10:05:06 +1100

    Profiles, simpler
    split for future deployment

---
+++ PAYLOAD/AppleProfilesVPNDaemon.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# AppleProfiles : Payload required. Profiles.
+# chris.gerke@gmail.com
+
+PROFILE="VPN"
+DAEMON="AppleProfiles${PROFILE}"
+
+# // fix
+ROOT="$3"; if [ -z "${ROOT}" ] || [ "${ROOT}" = "/" ]; then ROOT=""; fi
+
+# Payload
+[ -e "$1/Contents/Resources/PAYLOAD/${PROFILE}.mobileconfig" ] && sudo cp -Rf "$1/Contents/Resources/PAYLOAD/" "${ROOT}/usr/local/bin" || exit 1
+
+# LaunchDaemon
+sudo defaults write "${ROOT}/Library/LaunchDaemons/${DAEMON}" Label "com.cmg.${DAEMON}"
+sudo defaults write "${ROOT}/Library/LaunchDaemons/${DAEMON}" RunAtLoad -bool TRUE
+sudo defaults write "${ROOT}/Library/LaunchDaemons/${DAEMON}" ProgramArguments -array "/bin/sh" "-c" "/usr/local/bin/${DAEMON}"
+
+# Permissions
+[ -e "${ROOT}/Library/LaunchDaemons/${DAEMON}.plist" ] && sudo chown root:wheel "${ROOT}/Library/LaunchDaemons/${DAEMON}.plist"
+[ -e "${ROOT}/Library/LaunchDaemons/${DAEMON}.plist" ] && sudo chmod 644 "${ROOT}/Library/LaunchDaemons/${DAEMON}.plist"
+
+# Load if booted
+if [ -z "${ROOT}" ] || [ "${ROOT}" = "" ]; then sudo launchctl load -w "${ROOT}/Library/LaunchDaemons/${DAEMON}.plist"; fi
+
+exit 0
