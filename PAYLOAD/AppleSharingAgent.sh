c2e52f0abe87c2425eba8bce93be204769008aca
Chris Gerke <chris.gerke@gmail.com>
2012-10-11 10:15:56 +1100

    simplify

--- PAYLOAD/AppleSharingAgent.sh
+++ PAYLOAD/AppleSharingAgent.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # AppleSharing : Payload required. Apple Remote Assistance.
 # chris.gerke@gmail.com
 
@@ -7,41 +7,37 @@ AGENT="AppleSharing"
 # // fix
 ROOT="$3"; if [ -z "${ROOT}" ] || [ "${ROOT}" = "/" ]; then ROOT=""; fi
 
-# Admin check
-sudo cat > "${ROOT}/usr/local/bin/ard" << EOPROFILE
-#!/bin/sh
-# ard : Enable sharing for admins, configure sharing for end users.
-
-# Enable Remote Management automatically for admins otherwise configure it so end users have to accept a connection
-/usr/bin/dsmemberutil checkmembership -U "\${USER}" -G admin | /usr/bin/grep "not"
-if [ "\$?" == "0" ]; then
- /usr/local/bin/ardoff
-else
- /usr/local/bin/ardon
-fi
-
-exit 0
-EOPROFILE
+# Payload
+[ -e "$1/Contents/Resources/PAYLOAD/ard" ] && sudo cp -f "$1/Contents/Resources/PAYLOAD/ard" "${ROOT}/usr/local/bin/ard" || exit 1
 
 # Permissions
-sudo chown root:wheel "${ROOT}/usr/local/bin/ard"
-sudo chmod 755 "${ROOT}/usr/local/bin/ard"
-sudo chmod u+s "${ROOT}/usr/local/bin/ard"
+[ -e "${ROOT}/usr/local/bin/ard" ] && sudo chown root:wheel "${ROOT}/usr/local/bin/ard"
+[ -e "${ROOT}/usr/local/bin/ard" ] && sudo chmod 755 "${ROOT}/usr/local/bin/ard"
+[ -e "${ROOT}/usr/local/bin/ard" ] && sudo chmod u+s "${ROOT}/usr/local/bin/ard"
 
-# Detect payload & copy
-if [ -r "$1/Contents/Resources/PAYLOAD" ]; then
- # Target
- sudo mkdir -p "${ROOT}/usr/local/bin"
- # Payload
- sudo cp -Rf "$1/Contents/Resources/PAYLOAD/" "${ROOT}/usr/local/bin"
- # Permissions
- sudo chown root:wheel "${ROOT}/usr/local/bin/ardoff"
- sudo chmod u+s "${ROOT}/usr/local/bin/ardoff"
- sudo chown root:wheel "${ROOT}/usr/local/bin/ardon"
- sudo chmod u+s "${ROOT}/usr/local/bin/ardon"
- sudo chown root:wheel "${ROOT}/usr/local/bin/sshon"
- sudo chmod u+s "${ROOT}/usr/local/bin/sshon"
-fi
+# Payload
+[ -e "$1/Contents/Resources/PAYLOAD/ardon" ] && sudo cp -f "$1/Contents/Resources/PAYLOAD/ardon" "${ROOT}/usr/local/bin/ardon" || exit 1
+
+# Permissions
+[ -e "${ROOT}/usr/local/bin/ardon" ] && sudo chown root:wheel "${ROOT}/usr/local/bin/ardon"
+[ -e "${ROOT}/usr/local/bin/ardon" ] && sudo chmod 755 "${ROOT}/usr/local/bin/ardon"
+[ -e "${ROOT}/usr/local/bin/ardon" ] && sudo chmod u+s "${ROOT}/usr/local/bin/ardon"
+
+# Payload
+[ -e "$1/Contents/Resources/PAYLOAD/ardoff" ] && sudo cp -f "$1/Contents/Resources/PAYLOAD/ardoff" "${ROOT}/usr/local/bin/ardoff" || exit 1
+
+# Permissions
+[ -e "${ROOT}/usr/local/bin/ardoff" ] && sudo chown root:wheel "${ROOT}/usr/local/bin/ardoff"
+[ -e "${ROOT}/usr/local/bin/ardoff" ] && sudo chmod 755 "${ROOT}/usr/local/bin/ardoff"
+[ -e "${ROOT}/usr/local/bin/ardoff" ] && sudo chmod u+s "${ROOT}/usr/local/bin/ardoff"
+
+# Payload
+[ -e "$1/Contents/Resources/PAYLOAD/sshon" ] && sudo cp -f "$1/Contents/Resources/PAYLOAD/sshon" "${ROOT}/usr/local/bin/sshon" || exit 1
+
+# Permissions
+[ -e "${ROOT}/usr/local/bin/sshon" ] && sudo chown root:wheel "${ROOT}/usr/local/bin/sshon"
+[ -e "${ROOT}/usr/local/bin/sshon" ] && sudo chmod 755 "${ROOT}/usr/local/bin/sshon"
+[ -e "${ROOT}/usr/local/bin/sshon" ] && sudo chmod u+s "${ROOT}/usr/local/bin/sshon"
 
 # LaunchAgent
 sudo defaults write "${ROOT}/Library/LaunchAgents/${AGENT}" Label "com.cmg.${AGENT}"
@@ -49,8 +45,8 @@ sudo defaults write "${ROOT}/Library/LaunchAgents/${AGENT}" RunAtLoad -bool TRUE
 sudo defaults write "${ROOT}/Library/LaunchAgents/${AGENT}" ProgramArguments -array "/bin/sh" "-c" "/usr/local/bin/ard"
 
 # Permissions
-sudo /usr/sbin/chown root:wheel "${ROOT}/Library/LaunchAgents/${AGENT}.plist"
-sudo /bin/chmod 644 "${ROOT}/Library/LaunchAgents/${AGENT}.plist"
+[ -e "${ROOT}/Library/LaunchAgents/${AGENT}.plist" ] && sudo /usr/sbin/chown root:wheel "${ROOT}/Library/LaunchAgents/${AGENT}.plist"
+[ -e "${ROOT}/Library/LaunchAgents/${AGENT}.plist" ] && sudo /bin/chmod 644 "${ROOT}/Library/LaunchAgents/${AGENT}.plist"
 
 # Load if booted
 if [ -z "${ROOT}" ] || [ "${ROOT}" = "" ]; then sudo launchctl load -w "${ROOT}/Library/LaunchAgents/${AGENT}.plist"; fi
